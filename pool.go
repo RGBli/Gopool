6d7d183e78c16a7db7aa0b5b96462e86c6edbe79
lbw <2362995697@qq.com>
2023-01-20 23:52:28 +0800

    feat: go routine first commit

---
+++ pool.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var ErrScheduleTimeout = fmt.Errorf("schedule err: timeout")
+
+type Pool struct {
+	semCh  chan struct{}
+	workCh chan func()
+}
+
+func NewPoolLazyWorker(size int64) *Pool {
+	p := &Pool{
+		workCh: make(chan func()),
+		semCh:  make(chan struct{}, size),
+	}
+
+	return p
+}
+
+func NewPoolHungryWorker(size, preload int64) *Pool {
+	if preload > size {
+		panic("preload goroutines more than total goroutines")
+	}
+
+	p := &Pool{
+		workCh: make(chan func()),
+		semCh:  make(chan struct{}, size),
+	}
+
+	var i int64
+	for i = 0; i < preload; i++ {
+		p.semCh <- struct{}{}
+		go p.worker(func() {})
+	}
+
+	return p
+}
+
+func (p *Pool) Schedule(task func()) error {
+	return p.schedule(task, nil)
+}
+
+func (p *Pool) ScheduleWithTimeout(task func(), timeout time.Duration) error {
+	return p.schedule(task, time.After(timeout))
+}
+
+func (p *Pool) schedule(task func(), timeout <-chan time.Time) error {
+	select {
+	case <-timeout:
+		return ErrScheduleTimeout
+	case p.workCh <- task:
+	case p.semCh <- struct{}{}:
+		go p.worker(task)
+	}
+
+	return nil
+}
+
+func (p *Pool) worker(task func()) {
+	defer func() {
+		<-p.semCh
+	}()
+
+	for {
+		task()
+		task = <-p.workCh
+	}
+}
